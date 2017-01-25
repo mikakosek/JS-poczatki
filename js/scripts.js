1041a68d926673ea8e57d0809e487515fededbf2
Monika Kosek <mika.kosek@gmail.com>
2017-01-25 12:35:07 +0100

    zadanie 8.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+/* pierwszy skrypt JS w kodilli */
 var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
+// alert('Witaj, ' + name);
 console.log('Witaj , ' + name);
+
+/* tutaj niedługo będzie kod pola trójkąta */
