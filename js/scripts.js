3f31de7fec22bfaad3a39687422bd688d729ead9
Monika Kosek <mika.kosek@gmail.com>
2017-01-25 15:22:55 +0100

    zadanie 8.5

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,23 @@ var name = prompt('Wpisz swoje imię');
 // alert('Witaj, ' + name);
 console.log('Witaj , ' + name);
 
-/* pole trójkąta */
+/* a może jednak sprawdzimy która liczba jest większa? */
 var a = Math.floor((Math.random() * 100) + 1),
-    h = Math.floor((Math.random() * 100) + 1),
-    triangleArea = a * h / 2;
+    b = Math.floor((Math.random() * 100) + 1),
+    value = (a * a) + (2 * a * b) - (b * b),
+    info = "Wynik działania (a*a)+(2*a*b)-(b*b) dla liczb a=" + a +" i b=" + b + " wynosi " + value + ". ";
 
-console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+switch (Math.sign(value)) {
+    case -1:
+        info += "Jest to liczba ujemna.";
+        break;
+    case 1:
+        info += "Jest to liczba dodatnia.";
+        break;
+    case 0:
+        info += "Jest to zero.";
+        break;
+}
+
+console.log(info);
+
