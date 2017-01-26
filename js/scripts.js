dc157515946175af4eff1690555b57e9387f93a4
Monika Kosek <mika.kosek@gmail.com>
2017-01-26 12:50:11 +0100

    zadanie 8.8 - napisy

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,5 @@
-/* pierwszy skrypt JS w kodilli */
-var name = prompt('Wpisz swoje imię');
-// alert('Witaj, ' + name);
-console.log('Witaj , ' + name);
+/* czas na stringi :) */
+var animal = "Zielone słonie".toUpperCase(),
+    text = "Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.".replace("Papugi", animal);
 
-/* funkcja wyliczająca pole trójkąta */
-function getTriangleArea(a, h) {
-    if (a <= 0 || h <= 0) {
-        return "Nieprawidłowe dane";
-    }
-    
-    return a * h / 2;
-}
-
-var triangle1Area = getTriangleArea(Math.floor((Math.random() * 100) + 1), Math.floor((Math.random() * 100) + 1)),
-    triangle2Area = getTriangleArea(Math.floor((Math.random() * 100) + 1), Math.floor((Math.random() * 100) + 1)),
-    triangle3Area = getTriangleArea(Math.floor((Math.random() * 100) + 1), Math.floor((Math.random() * 100) + 1));
-
-console.log('Pola trójkątów wynoszą: ' + triangle1Area + ', ' + triangle2Area + ' i ' + triangle3Area);
+console.log(text.substring(0,text.length/2));
