a75645803522a4b189ed7597a1fbe8b7984a238a
Monika Kosek <mika.kosek@gmail.com>
2017-01-26 12:34:53 +0100

    zadanie 8.7 tablice

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,12 @@
-/* pierwszy skrypt JS w kodilli */
-var name = prompt('Wpisz swoje imię');
-// alert('Witaj, ' + name);
-console.log('Witaj , ' + name);
+/* łączenie tablic */
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'],
+    maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'],
+    allNames = femaleNames.concat(maleNames);
 
-/* funkcja wyliczająca pole trójkąta */
-function getTriangleArea(a, h) {
-    if (a <= 0 || h <= 0) {
-        return "Nieprawidłowe dane";
-    }
-    
-    return a * h / 2;
+var newName = prompt("Podaj nowe imię");
+
+if (allNames.indexOf(newName) === -1) {
+    allNames.push(newName);
 }
 
-var triangle1Area = getTriangleArea(Math.floor((Math.random() * 100) + 1), Math.floor((Math.random() * 100) + 1)),
-    triangle2Area = getTriangleArea(Math.floor((Math.random() * 100) + 1), Math.floor((Math.random() * 100) + 1)),
-    triangle3Area = getTriangleArea(Math.floor((Math.random() * 100) + 1), Math.floor((Math.random() * 100) + 1));
-
-console.log('Pola trójkątów wynoszą: ' + triangle1Area + ', ' + triangle2Area + ' i ' + triangle3Area);
+console.log(allNames.sort());
